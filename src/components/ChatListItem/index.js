9079b0d781b3edd10a83f4e673cfcb411f9f78d7
Saptak Bose <ranicob2014@gmail.com>
2023-03-19 18:42:11 +0530

    Props Done

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 
-export default function ChatListItem() {
+export default function ChatListItem({ chat: { id, user, lastMessage } }) {
   return (
     <View className="theContainer flex flex-row mx-[10px] my-[5px] h-[70px]">
       <Image
         className="theImage w-[60px] h-[60px] rounded-full mr-[10px]"
         source={{
-          uri: "https://firebasestorage.googleapis.com/v0/b/storage-15b1a.appspot.com/o/CleverSap%20Profile.jpg?alt=media&token=ad2b0772-c4f9-4321-8e83-ebcc2735016e",
+          uri: user.image,
         }}
       />
 
       <View className="theContent flex-1 border-b-[0.4px] border-[#d3d3d3]">
         <View className="theRow flex flex-row mb-[5px]">
           <Text numberOfLines={1} className="theName flex-1 font-bold">
-            Saptak
+            {user.name}
           </Text>
-          <Text className="theSubtitle text-[#808080]">8:30</Text>
+          <Text className="theSubtitle text-[#808080]">
+            {lastMessage.createdAt}
+          </Text>
         </View>
 
         <Text numberOfLines={2} className="theSubtitle text-[#808080]">
-          Hello there
+          {lastMessage.text}
         </Text>
       </View>
     </View>
